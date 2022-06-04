4bd5382c55e84a3acaac8aee13d4e12f5501812a
Angelina <angelinarwhite@gmail.com>
2022-06-04 19:08:38 -0400

    pass tests

--- looping.rb
+++ looping.rb
@@ -1,5 +1,11 @@
 def happy_new_year
   # your code here
+  i = 10
+  while i > 0
+    puts i
+    i -= 1
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +23,29 @@ end
 
 def fizzbuzz_printer
   # your code here
+  i=1
+  while i<=100
+    if i%3==0 && i%5==0
+      puts "FizzBuzz"
+    elsif i%3==0
+      puts "Fizz"
+    elsif i%5==0
+      puts "Buzz"
+    else
+      puts i
+    end
+    i+=1
+  end
 end
 
 def reverse_string(str)
   # your code here
+  arr=[]
+  i = 0
+  while i < str.length
+    arr.unshift(str[i])
+    i += 1
+  end
+  newArr = arr.join()
+  return newArr
 end
